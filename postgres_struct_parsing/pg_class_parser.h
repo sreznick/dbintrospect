b5dafe2e4a9ea538872bb267f2ed0f9b388883dd
Mikhail Sladkov <msladkov2002@gmail.com>
2024-12-20 13:20:57 +0300

    added dump function

--- postgres_struct_parsing/pg_class_parser.h
+++ postgres_struct_parsing/pg_class_parser.h
@@ -4,4 +4,5 @@
 
 #include <stdio.h>
 
-void parse_pg_class(FILE* f, struct pg_class* dst);
+void parse_pg_class(FILE* f, struct pg_class* const dst);
+void dump_pg_class(FILE* f, const struct pg_class* const src);
--- postgres_struct_parsing/pg_class_parser_test.c
+++ postgres_struct_parsing/pg_class_parser_test.c
@@ -1,46 +1,23 @@
 #include <stdio.h>
 #include <string.h>
-// #include "pg_class_parser.h"
+#include "pg_class_parser.h"
 
-int main() {
-    FILE* pg_class_file = fopen("example/1259_13797", "rb");
-    // struct pg_class to_read;
-    // parse_pg_class(pg_class_file, &to_read);
-    FILE* chardump = fopen("chardump.txt", "w");
+int main(int argc, char** argv) {
+    char* fname;
+    if (argc > 1) {
+        fname = argv[1];
+    } else {
+        fname = "example/1259";
+    }
+    FILE* pg_class_file = fopen(fname, "rb");
+    if (pg_class_file == NULL) {
+        return -1;
+    }
+    struct pg_class to_read;
+    fseek(pg_class_file, MAGIC_OFFSET, SEEK_SET);
+    parse_pg_class(pg_class_file, &to_read);
     char buf[32];
     long counter = 0;
-    while(fread(buf, 1, 32, pg_class_file) == 32) {
-        for (int i = 0; i < 32; ++i) {
-            if (!((buf[i] >= 'a' && buf[i] <= 'z') || (buf[i] >= 'A' && buf[i] <= 'Z') || (buf[i] >= '0' && buf[i] <= '9') || (buf[i] == '_'))) {
-                buf[i] = '.';
-            }
-        }
-        buf[31] = 0;
-        if (strstr(buf, "pg_class.") != NULL) {
-            printf("%ld %s\n", counter, buf);
-        }
-        fwrite(buf, 1, 1, chardump);
-        fseek(pg_class_file, -32, SEEK_CUR);
-        int t;
-        fread(&t, sizeof(int), 1, pg_class_file);
-        fseek(pg_class_file, -sizeof(int) + 1, SEEK_CUR);
-
-        if (t == 1259) {
-            printf("1259 at %ld\n", counter);
-        }
-        if (counter % 1024 == 1) {
-            printf("I am alive\n");
-        }
-        if (counter % 128 == 0) {
-            fwrite("\n", 1, 1, chardump);
-        }
-        if (counter % 8192 == 0) {
-            printf("\n");
-            fwrite("\n", 1, 1, chardump);
-        }
-        counter += 1;
-    }
-    fclose(chardump);
     fclose(pg_class_file);
-    // printf("Oid: %d, relname: %s\n", to_read.oid, to_read.relname);
+    dump_pg_class(stdout, &to_read);
 }
